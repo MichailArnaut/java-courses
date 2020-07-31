a72b688d0321f5f6d599cb0b2de87577ab6017a5
Михаил Арнаут <arnautmv@sup.local>
2020-07-31 13:32:29 +0300

    Calculate add

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,18 @@ public class Calculate {
 
        public static void main(String[] arg) {
              System.out.println("Calculate...");
+             int first = Integer.valueOf(arg[0]);
+             int second = Integer.valueOf(arg[1]);
+             int summ = first + second;
+             int raznost = first - second;
+             int umnog = first*second;
+             double delit = first/second;
+             int step = first*first;
+             System.out.println("Sum " + summ);
+             System.out.println("Raznost "  + raznost);
+             System.out.println("Umnog " + umnog);
+             System.out.println("Delit " + delit);
+             System.out.println("Step " + step);
        
        }
 
